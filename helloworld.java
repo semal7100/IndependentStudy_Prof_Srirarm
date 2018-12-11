44ebbdf14e1af1720bb7ff264a5b608e8a118f3c
Shemal Somil Lalaji <shla8652@colorado.edu>
2018-12-11 15:31:19 -0700

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,8 +0,0 @@
-public class helloworld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
